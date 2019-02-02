659889a4fed35a214adca079f4323ecfe9b7c3c7
farshad jahanmanesh <jahanmanesh@hamisystem.ir>
2019-02-02 15:14:50 +0330

    Initial Commit

---
+++ loady/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  loady
+//
+//  Created by farshad jahanmanesh on 2/2/19.
+//  Copyright © 2019 farshadJahanmanesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
