4232265bf9d11b39192df3a95d352f7e6c1b9810
Farshad Jahanmanesh <farshadjahanmanesh@gmail.com>
2019-11-06 16:42:51 +0330

    animations added in seperate files

---
+++ loady/LoadingButton/Animations/LoadyTopLineAnimation.swift
@@ -0,0 +1,9 @@
+//
+//  LoadyTopLineAnimation.swift
+//  loady
+//
+//  Created by Farshad Jahanmanesh on 11/6/19.
+//  Copyright © 2019 farshadJahanmanesh. All rights reserved.
+//
+
+import Foundation
